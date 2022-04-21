a614661dd89b876f3aecdaa4b5db6392f251eaa5
Sandhya-Akula <84383268+Sandhya-Akula@users.noreply.github.com>
2022-04-21 20:41:21 +0530

    def prime(n):     fc=0     for i in range(1,n+1):         if n%i==0:             fc+=1     return fc
    n=int(input())
    fc=prime(n)
    if fc==2:
        print("prime")
    else:
        print("not a prime")

---
+++ prime_number.py
@@ -0,0 +1,13 @@
+def prime(n):
+    fc=0
+    for i in range(1,n+1):
+        if n%i==0:
+            fc+=1
+    return fc
+        
+n=int(input())
+fc=prime(n)
+if fc==2:
+    print("prime")
+else:
+    print("not a prime")
