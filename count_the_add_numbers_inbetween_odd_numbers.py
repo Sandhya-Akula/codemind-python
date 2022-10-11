a9c2a482d6bed77c62557ec1b91f945b2e3240ca
Sandhya-Akula <84383268+Sandhya-Akula@users.noreply.github.com>
2022-10-11 10:42:06 +0530

    n=int(input()) l=list(map(int,input().split()))[:n] ans=0 for i in range(0,len(l)-2):     if l[i]%2!=0 and l[i+2]%2!=0:         if l[i+1]%2!=0:             ans+=1
    print(ans)

---
+++ count_the_add_numbers_inbetween_odd_numbers.py
@@ -0,0 +1,10 @@
+n=int(input())
+l=list(map(int,input().split()))[:n]
+ans=0
+for i in range(0,len(l)-2):
+    if l[i]%2!=0 and l[i+2]%2!=0:
+        if l[i+1]%2!=0:
+            ans+=1
+    
+    
+print(ans)
